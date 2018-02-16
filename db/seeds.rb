498863a53eab1fd9d738ec0926e6674d586eceec
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-02-15 22:12:42 -0600

    Genres table working and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,22 @@ csv_filename = Rails.root + 'db/anime_database_sample.csv'
 # xml_file = Nokogiri::XML(Rails.root + 'db/season_data/2017_fall.xml')
 
 options = { key_mapping: { anime_id: :mal_id, episodes: :episode_count,
-                           genre: nil, type: nil, members: nil } }
+                           type: nil, members: nil } }
 
 animes = SmarterCSV.process(csv_filename, options)
 
-animes[0..99].each do |a|
-  anime = Anime.create(a)
+animes[0..99].each do |anime_item|
+  csv_genres = anime_item[:genre].split(',').map(&:strip).compact
+
+  # Since we're no longer 'nilling' the genre column
+  anime_item.delete(:genre)
+
+  anime = Anime.create(anime_item)
+
+  csv_genres.each do |genre_name|
+    # Creates a Genres row from the genre.name, if it doesn't exist already
+    genre = Genre.find_or_create_by(name: genre_name)
+    AnimeGenre.create(anime: anime, genre: genre)
+  end
 end
 

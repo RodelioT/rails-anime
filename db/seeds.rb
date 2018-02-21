444cd743816b548bef09ac75fc3f8dc28a069906
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-02-21 08:16:16 -0600

    No longer crashes when an Anime has no Genres
    Checks first if an Anime has at least one Genre. If not, it is given a genre of 'unlisted'

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,16 @@ Anime.delete_all
 Genre.delete_all
 
 csv_filename = Rails.root + 'db/anime_database_sample.csv'
-xml_file = Nokogiri::XML(File.read(Rails.root + 'db/season_data/2016_summer.xml'))
 
 options = { key_mapping: { anime_id: :mal_id, episodes: :episode_count,
                            type: nil, members: nil } }
 animes = SmarterCSV.process(csv_filename, options)
 
-animes[0..255].each do |anime_item|
+animes.each do |anime_item|
+  if anime_item[:genre].nil?
+    anime_item[:genre] = 'unlisted'
+  end
+
   csv_genres = anime_item[:genre].split(',').map(&:strip).compact
 
   # Since we're no longer 'nilling' the genre column
@@ -28,17 +31,26 @@ end
 puts 'finish'
 
 
-# Creates a hash of hashes with the format: "32574" => {:producers=>"TOHO animation, Bones",
-#                                                       :image_url=>"https://myanimelist.cdn-dena.com/images/anime/10/79209.jpg"}
-season_data_hash = {}
-
-array = xml_file.css('season anime')
-array[0..2].each do |anime|
-  producers_node = anime.css('producers').text
-  image_node = anime.css('image').text
-  id_node = anime.css('id').text
-
-  season_data_hash[id_node] = { producers: producers_node, image_url: image_node }
-end
-
-puts season_data_hash
+# # Creates a hash of hashes to store all season data with the format:
+# # {"32574" => {:producers=>"TOHO animation, Bones",
+# #              :image_url=>"https://myanimelist.cdn-dena.com/images/anime/10/79209.jpg"}, ...}
+# season_data_hash = {}
+#
+# # Array of all XML file names within the season_data folder
+# xml_file_array = Dir.glob(File.join(Rails.root, 'db', 'season_data', '*'))
+#
+# xml_file_array.each do |xml_file_name|
+#   xml_file = Nokogiri::XML(File.read(xml_file_name))
+#
+#   # Array of all 'Anime' Tags within the currently processed file
+#   array = xml_file.css('season anime')
+#   array.each do |anime|
+#     producers_node = anime.css('producers').text
+#     image_node = anime.css('image').text
+#     id_node = anime.css('id').text
+#
+#     season_data_hash[id_node] = { producers: producers_node, image_url: image_node }
+#   end
+# end
+#
+# puts season_data_hash
